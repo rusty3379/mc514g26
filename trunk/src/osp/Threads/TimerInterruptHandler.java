a09948f657e960bc351f75a5106529d05ffdb991
wsgomes <wsgomes@6773003f-ac95-2f34-4ec2-07e59627fa21>
2010-09-30 20:55:37 +0000

    Formatação e edição final. Alterei para GenericList pois não consegui rodar corretamente com ArrayList (provavelmente por algum erro meu mas, se quiser tentar com ArrayList, manda bala). Fiz de novo o suspend e o dispatch pois me enrolei tbm com o que vc tinha feito. Rodei aqui e está funcionando corretamente. Desculpa por alterar coisas que vc tinha feito. Eu sou meio ruim pra mexer em código pronto e acabo refazendo. Abs

--- trunk/src/osp/Threads/TimerInterruptHandler.java
+++ trunk/src/osp/Threads/TimerInterruptHandler.java
@@ -1,38 +1,43 @@
+/*
+* Grupo 26
+* RA: 092549 - Paolo Barreto Nunes da Silva 
+* RA: 097107 - Wellington da Silva Gomes
+*
+* Status: Final
+*
+* 30/09/2010
+* 1. Alteração 1 - Estamos estudando o projeto
+* 2. Alteração 2 - Estamos estudando o projeto, fazendo as devidas leituras do OSP2
+* 3. Alteração 3 - Finalização do projeto
+*/
+
 package osp.Threads;
 
 import osp.IFLModules.*;
 import osp.Utilities.*;
 import osp.Hardware.*;
 
-/**    
-       The timer interrupt handler.  This class is called upon to
-       handle timer interrupts.
-
-       @OSPProject Threads
+/**
+* The timer interrupt handler.  This class is called upon to handle timer
+* interrupts.
 */
-public class TimerInterruptHandler extends IflTimerInterruptHandler
-{
-    /**
-       This basically only needs to reset the times and dispatch
-       another process.
-
-       @OSPProject Threads
-    */
-    public void do_handleInterrupt()
-    {
-    	MyOut.print(this, " ====>>> timer");
-        HTimer.set(10);
-        ThreadCB.dispatch();
-
-    }
-
-
-    /*
-       Feel free to add methods/fields to improve the readability of your code
-    */
-
+public class TimerInterruptHandler extends IflTimerInterruptHandler {
+	
+	/**
+	* This basically only needs to reset the times and dispatch another
+	* process.
+	*/
+	public void do_handleInterrupt() {
+		MyOut.print(this, " ====>>> timer");
+		HTimer.set(10);
+		ThreadCB.dispatch();
+	}
+	
+	/*
+	* Feel free to add methods/fields to improve the readability of your code
+	*/
 }
 
 /*
-      Feel free to add local classes to improve the readability of your code
+* Feel free to add local classes to improve the readability of your code
 */
